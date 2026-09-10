1b00de93a29941fed788668ea1ed4ed5094235ec
henrickaraujo18 <henrickaraujo2009@gmail.com>
2026-09-10 00:44:52 -0300

    Adicionar configuração pública de autenticação

---
+++ desktop-dist/config-v16.js
@@ -0,0 +1,9 @@
+window.FS_CONFIG = Object.freeze({
+  authRequired: false,
+  supabaseUrl: "",
+  supabasePublishableKey: "",
+  passwordResetUrl: "",
+  accessFunction: "workspace-access",
+  syncEnabled: true,
+  updateCheckOnReconnect: true,
+});
