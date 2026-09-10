1e714304139c65da1532c1d04ffc65220891f601
henrickaraujo18 <henrickaraujo2009@gmail.com>
2026-09-10 00:50:05 -0300

    Remover implementação duplicada de configuração de autenticação

--- desktop-dist/config-v16.js
+++
@@ -1,9 +0,0 @@
-window.FS_CONFIG = Object.freeze({
-  authRequired: false,
-  supabaseUrl: "",
-  supabasePublishableKey: "",
-  passwordResetUrl: "",
-  accessFunction: "workspace-access",
-  syncEnabled: true,
-  updateCheckOnReconnect: true,
-});
